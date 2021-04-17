2a6feef32b437ad7b0a4e73db29a8519cc2512c5
ikedam <devld@ikedam.jp>
2021-04-17 15:33:03 +0900

    Remove ArduinoOTA feature

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <M5EPD.h>
-#include <ArduinoOTA.h>
+#include <Wifi.h>
 #include "SHT3X.h"
 #include "WiFiInfo.h"
 
@@ -182,41 +182,8 @@ void setup(void)
     delay(WAIT_ON_FAILURE);
   }
 
-  ArduinoOTA
-      .onStart([]() {
-        String type;
-        if (ArduinoOTA.getCommand() == U_FLASH)
-          type = "sketch";
-        else // U_SPIFFS
-          type = "filesystem";
-
-        // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-        Serial.println("Start updating " + type);
-      })
-      .onEnd([]() {
-        Serial.println("\nEnd");
-      })
-      .onProgress([](unsigned int progress, unsigned int total) {
-        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-      })
-      .onError([](ota_error_t error) {
-        Serial.printf("Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR)
-          Serial.println("Auth Failed");
-        else if (error == OTA_BEGIN_ERROR)
-          Serial.println("Begin Failed");
-        else if (error == OTA_CONNECT_ERROR)
-          Serial.println("Connect Failed");
-        else if (error == OTA_RECEIVE_ERROR)
-          Serial.println("Receive Failed");
-        else if (error == OTA_END_ERROR)
-          Serial.println("End Failed");
-      });
-
-  ArduinoOTA.begin();
-
-  // env2 unit
-  if (!sht30.begin(21, 22, 400000))
+  // built-in sht30
+  if (!sht30.begin())
   {
     gfx.println("Failed to initialize external I2C");
   }
@@ -246,7 +213,6 @@ void loop(void)
   static uint32_t cnt = TIME_SYNC_CYCLE - 3;
 
   xSemaphoreTake(xMutex, portMAX_DELAY);
-  ArduinoOTA.handle();
 
   float tmp = 0.0;
   float hum = 0;
